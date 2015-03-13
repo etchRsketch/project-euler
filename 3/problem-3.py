9bf10b9cacbdefc1742bc9785d8cf178de2aa299
esteban <stephendavidboyd@gmail.com>
2015-03-13 03:01:16 -0700

    problem 3

---
+++ 3/problem-3.py
@@ -0,0 +1,22 @@
+#What is the largest prime factor of the number 600851475143 ?
+import math
+
+def largestPrimeFactor(number):
+  divisor = isComposite(number)
+  if divisor:
+    quotient = number / divisor
+    print str(number) + "/ " + str(divisor) + " = " + str(quotient)
+    largestPrimeFactor(quotient)
+  else:
+    print str(number) + " is prime"
+    return number
+
+def isComposite (number):
+  for x in range(2, int(math.sqrt(number))):
+    if number % x == 0:
+      return x
+  return False
+
+q = 600851475143
+print ""
+print largestPrimeFactor(q)
